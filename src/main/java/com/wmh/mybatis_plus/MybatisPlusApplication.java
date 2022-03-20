b5577e5f28c406162571635f90370b3feb79950c
JPower <1152151276@JPower.com>
2022-03-20 22:46:02 +0800

    first commit-51221

--- src/main/java/com/wmh/mybatis_plus/MybatisPlusApplication.java
+++ src/main/java/com/wmh/mybatis_plus/MybatisPlusApplication.java
@@ -16,7 +16,7 @@ public class MybatisPlusApplication {
         SpringApplication.run(MybatisPlusApplication.class, args);
         System.out.println("sdf");
         System.out.println("hot-fix");
-        System.out.println("sb");
+        System.out.println("sb123123");
         System.out.println("大SB1");
     }
 
