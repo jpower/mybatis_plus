480beafee15e1989ff747d4fd012d5fb895cfee5
JPower <1152151276@JPower.com>
2022-03-20 22:50:28 +0800

    first commit-51221345312235345234

--- src/main/java/com/wmh/mybatis_plus/MybatisPlusApplication.java
+++ src/main/java/com/wmh/mybatis_plus/MybatisPlusApplication.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 
 @SpringBootApplication
 public class MybatisPlusApplication {
+
     public void fun1(){
         System.out.println("大SB22222");
     }
