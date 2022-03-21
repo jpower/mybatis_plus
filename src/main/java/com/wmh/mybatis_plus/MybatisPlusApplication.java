c1a66fe2d4de4c52ba505acdab13f47baa38e62b
wangmh <1152151276@qq.com>
2022-03-21 14:43:06 +0800

    123444

--- src/main/java/com/wmh/mybatis_plus/MybatisPlusApplication.java
+++ src/main/java/com/wmh/mybatis_plus/MybatisPlusApplication.java
@@ -14,7 +14,7 @@ public class MybatisPlusApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MybatisPlusApplication.class, args);
-        System.out.println("sd232233");
+        System.out.println("sd2333");
         System.out.println("hot-fix");
         System.out.println(818);
     }
